d060509cbbf01c82548746b7faf69e5d79b291e8
ElViny98 <viny.mtz@gmail.com>
2018-10-26 14:17:52 -0600

    Modal

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -88,30 +88,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
     -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+			<div class="modal-header">
+				<h6 class="modal-title" id="exampleModalLabel">Iniciar sesión</h6>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		<div class="modal-body">
+			<form class="form-signin">
+				<h1 class="h3 mb-3 font-weight-normal">Usuario</h1>
+				<label for="inputEmail" class="sr-only">Correo</label>
+				<input type="email" id="inputEmail" class="form-control" placeholder="Correo" required="" autofocus="">
+
+				<label for="inputPassword" class="sr-only">Contraseña</label>
+				<input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required="">
+				<br>
+			</form>
+		</div>
+		<div class="modal-footer">
+        	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        	<button type="button" class="btn btn-primary">Iniciar sesión</button>
+		</div>
+	</div>
+</div>
+</div>
+
   </body>
   <script type="text/javascript">
      function iniciarSesion(){
-         BootstrapDialog.show({
-             title: 'Iniciar Sesión',
-             message: `<form class="form-signin">
-               <h1 class="h3 mb-3 font-weight-normal">Usuario</h1>
-               <label for="inputEmail" class="sr-only">Correo</label>
-               <center><input type="email" id="inputEmail" class="form-control" placeholder="Correo" required="" autofocus=""></center>
-
-               <label for="inputPassword" class="sr-only">Contraseña</label>
-               <center><input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required=""></center>
-               <!--
-               <div class="checkbox mb-3">
-                   <label>
-                       <input type="checkbox" value="remember-me"> Remember me
-                   </label>
-               </div>
-                 -->
-                 <br>
-               <center><button id="btnIniciar" class="btn btn-lg btn-primary btn-block" type="submit">Iniciar</button></center>
-             </form>`,
-             type: BootstrapDialog.TYPE_PRIMARY
-         })
+		 $("#exampleModal").modal('show');
      }
   </script>
 </html>
