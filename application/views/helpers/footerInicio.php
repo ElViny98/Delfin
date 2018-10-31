e91684132d5387758ea3a235c4a202732fb5af3e
emmanuel <villager97@outlook.es>
2018-10-31 13:40:04 -0700

    contactus

--- application/views/helpers/footerInicio.php
+++ application/views/helpers/footerInicio.php
@@ -1,37 +1,40 @@
     <!-- Footer -->
     <footer class="page-footer font-small footer-dark">
-
-        <div class="container">
-          <!--Formulario de contacto-->
-          <div class="container">
-
-  <h2>Envianos un Mensaje</h2><!---->
+    <br>
+  <h2>Contactanos</h2><!---->
   <form class="form-horizontal" action="/action_page.php">
 
-  <div class="form-group">
-    <label class="control-label col-lg text-left" for="email">Email:</label>
-    <div class="col-sm-6">
-      <input type="email" class="form-control" id="email" placeholder="Enter email">
+  <div class="form-group" >
+
+<div class="row">
+  <div class="col-lg-6 col-md-6 col-sm-6" >
+    <div class="row">
+      <div class="col-lg-12">
+        <label class="control-label col-lg text-left" for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email">
+        <label class="control-label col-lg text-left" for="cnombre">Nombre Completo:</label>
+        <input type="cnombre" class="form-control" id="cnombre" placeholder="Nombre y Apellidos">
+        <label class="control-label col-lg text-left" for="cnombre">asunto:</label>
+        <input type="cnombre" class="form-control" id="cnombre" placeholder="Asunto">
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-sm-6">
+    <div class="row">
+      <div class="col-lg-12">
+        <label for="cmensaje">Mensaje:</label>
+        <textarea class="form-control" rows="7" id="cmensaje"></textarea>
+      </div>
     </div>
   </div>
 
-  <div class="form-group">
-    <label class="control-label col-lg text-left" for="cname">Nombre Completo:</label>
-    <div class="col-sm-6">
-      <input type="cnombre" class="form-control" id="cnombre" placeholder="Nombre y Apellidos">
-    </div>
-  </div>
+</div>
 
-</form>
-<form>
-  <div class="form-group">
-    <label for="comment">Comment:</label>
-    <textarea class="form-control" rows="5" id="comment"></textarea>
   </div>
 </form>
 
 <div class="col-lg ">
-  <button type="submit" class="btn btn-default">Submit</button>
+  <button type="cenviar" class="btn btn-default">Enviar</button>
 </div>
 
 </div><!--container-->
